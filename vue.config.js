ff71ad9f5fb06b29d5b2b2c6d967f7fc28d582f1
尾形嵐士 <53913051+o-ham5@users.noreply.github.com>
2019-09-05 18:15:20 +0900

    first commit

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  devServer: {
+    host: "localhost",
+    port: 8081,
+    disableHostCheck: true
+  },
+
+  pluginOptions: {
+    express: {
+      shouldServeApp: true,
+      serverDir: "./srv"
+    }
+  }
+};
