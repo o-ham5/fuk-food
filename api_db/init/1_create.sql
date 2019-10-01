0c82009e616eaba9cab4bb692b9b991f6d6644a1
fltwtn <tomoya.watanabe@firstloop-tech.com>
2019-10-01 17:41:20 +0900

    arrange development infrastructure

---
+++ api_db/init/1_create.sql
@@ -0,0 +1,12 @@
+use api_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    user VARCHAR(50),
+    mail VARCHAR(50),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (user, mail) VALUES ("test01", "test01@example.com");
+INSERT INTO users (user, mail) VALUES ("test02", "test02@example.com");
